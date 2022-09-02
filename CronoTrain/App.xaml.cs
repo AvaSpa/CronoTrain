c540246b865e43277623daa5d6e4f4885e1bf0f2
Adrian Cigher <adrian.cigher@gmail.com>
2022-09-02 17:33:06 +0300

    New Project

---
+++ CronoTrain/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CronoTrain;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CronoTrain/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Timer = System.Timers.Timer;
+
+namespace CronoTrain;
+
+public partial class MainPage : ContentPage
+{
+    private readonly Timer _timer;
+    private readonly Stopwatch _stopwatch;
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        _timer = new Timer();
+        _stopwatch = new Stopwatch();
+    }
+
+    private void OnStartClicked(object sender, EventArgs e)
+    {
+        _stopwatch.Start();
+    }
+
+    private void OnStopClicked(object sender, EventArgs e)
+    {
+        _stopwatch.Stop();
+        Timer.Text = _stopwatch.Elapsed.ToString("mm\\:ss\\.fff");
+    }
+}
+
